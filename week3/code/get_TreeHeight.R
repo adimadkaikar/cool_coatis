28619f5812ba9d9c11f4cc965eadb61280ee8637
Xuan Wang <xuan.wang22@imperial.ac.uk>
2022-12-12 00:00:13 +0000

    Tree height done

---
+++ week3/code/get_TreeHeight.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+library(tools)
+
+# loading data from the command line
+argv <- commandArgs(trailingOnly = TRUE)
+treedata <- read.csv(paste("../data/", argv[1], sep = ""))
+
+TreeHeight <- function(degrees, distance) {
+
+    radians <- degrees * pi / 180
+    height <- distance * tan(radians)
+
+    return(height)
+}
+
+for (Species in treedata) {
+    treehgt <- TreeHeight(treedata$Angle.degrees, treedata$Distance.m)
+    return(treehgt)
+} 
+
+## add the column of height to the data
+treedata$Tree.Height.m <- treehgt
+
+## select the first two rows of data
+height_selected <- treedata[1:2,]
+
+## saving the output file
+output <- basename(file_path_sans_ext(argv[1]))
+write.csv(height_selected, file = paste("../results/", output, "_treeheights.csv", sep = ""))
---
+++ week3/code/run_get_TreeHeight.sh
@@ -0,0 +1,6 @@
+# Testing of R file
+echo "R script started."
+Rscript get_TreeHeight.R trees.csv
+echo "R script done!"
+
+# Testing of python file
