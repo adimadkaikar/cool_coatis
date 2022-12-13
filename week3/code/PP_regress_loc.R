7c124016ad0810d4321a9138620c024694ad8311
adimadkaikar <arm122@ic.ac.uk>
2022-12-13 17:13:28 +0000

    Week 3 gitignores

---
+++ week3/code/PP_regress_loc.R
@@ -0,0 +1,87 @@
+# Cleaning the environment
+rm(list = ls())
+
+# Loading the required packages
+library(tidyverse)
+
+# Loading the data as MyDF
+MyDF <- read.csv("../data/EcolArchives-E089-51-D1.csv")
+
+# Converting the Prey mass to grams 
+MyDF$Prey.mass[MyDF$Prey.mass.unit == 'mg'] <- 
+    MyDF$Prey.mass[MyDF$Prey.mass.unit == 'mg']/1000
+
+# Building the plot
+g <- ggplot(MyDF, aes(x = log10(Prey.mass), y = log10(Predator.mass), 
+                      colour = Predator.lifestage)) +
+    geom_point(shape = I(3)) + 
+    geom_smooth(method = "lm", aes(colour=Predator.lifestage), fullrange=T)
+theme_classic()
+
+# Wrapping it for Type of feeding interaction
+g <- g + facet_wrap(.~Type.of.feeding.interaction, 
+                    ncol=1, strip.position = "right") 
+
+# Adding the legend
+g <- g + theme_bw() + theme(legend.position = "bottom") + 
+    guides(color = guide_legend(nrow = 1))
+
+# Setting the x-axis
+g <- g + scale_x_continuous(labels = c('1e-07', '1e-03', '1e+01'), 
+                            breaks = c(-7, -3, 1)) 
+
+# Setting the y-axis
+g <- g + scale_y_continuous(labels = c('1e-06', '1e-02', '1e+02', '1e+06'), 
+                            breaks = c(-6, -2, 2, 6)) 
+
+# Setting the axes names
+g <- g + xlab("Prey mass in grams") + ylab("Predator mass in grams")
+g
+
+# Printing the output to a PDF file
+pdf("../results/PP_Regress_plot.pdf", 11.7, 8.3)
+print(g)
+dev.off()
+
+# Creating a dataframe of required columns only
+new <- MyDF %>% select(Prey.mass, Predator.mass, 
+                       Type.of.feeding.interaction, Predator.lifestage,
+                       Location)
+
+# Creating an empty data frame
+new_df <- data.frame(Type.of.feeding.interaction = character(),
+                     Predator.lifestage = character(),
+                     Location = character(),
+                     Regression.slope = double(), 
+                     Regression.intercept = double(), 
+                     R.squared = double(), 
+                     F.statistic = double(),
+                     p.value = double())
+
+# Fitting linear regressions subset-wise and saving the required values to the 
+# initialized dataframe
+
+for(inter in unique(new$Type.of.feeding.interaction)){
+    for(lifestage in unique(new$Predator.lifestage)){
+        for(loc in unique(new$Location)){
+            tmp <- new %>%
+                filter(Type.of.feeding.interaction == inter,
+                       Predator.lifestage == lifestage,
+                       Location == loc)
+            if(nrow(tmp) > 2){ # Only fitting regression if there are more than 2 observations
+                mod <- lm(log10(Predator.mass)~log10(Prey.mass), data = tmp)
+                tmp1 <- summary(mod)
+                needed <- c(tmp1$coefficients[2,1],
+                            tmp1$coefficients[1,1],
+                            tmp1$adj.r.squared, 
+                            tmp1$fstatistic[1], 
+                            tmp1$coefficients[,4][2])
+                new_df[nrow(new_df)+1,] <- c(inter, lifestage, loc, needed)
+            }
+        }
+    }
+}
+
+
+# Writing the data frame to a results file
+write.csv(new_df, file = '../results/PP_Regress_Results.csv', row.names = F)
